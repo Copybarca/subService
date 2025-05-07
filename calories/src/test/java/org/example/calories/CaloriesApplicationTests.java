4d12417336d22baeb077fcc9e78d8b991d305785
Arthur <lizunov_artur@mail.ru>
2025-05-07 13:04:50 +0300

    add complex functionality to service

--- calories/src/test/java/org/example/calories/CaloriesApplicationTests.java
+++ calories/src/test/java/org/example/calories/CaloriesApplicationTests.java
@@ -11,32 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CaloriesApplicationTests {
 
-    @Autowired
-    DishService dishService;
-    @Autowired
-    UserService userService;
-    @Test
-    void contextLoads() {
-        var result = dishService.listAllDishes();
-        Assertions.assertFalse(result.isEmpty());
-    }
-    @Test
-    void contextLoads2() {
-        var result = dishService.findById(1);
-        Assertions.assertNotNull(result);
-    }
-    @Test
-    void saveTest() {
-        User user = new User();
-        user.setAge(22);
-        user.setHeight(178);
-        user.setWeight(64);
-        user.setTarget(Target.WEIGHT_UP);
-        user.setEmail("test@test.com");
-        user.setName("Art");
-        userService.createUser(user);
-        var result = dishService.findById(1);
-        Assertions.assertNotNull(result);
-    }
+
 
 }
